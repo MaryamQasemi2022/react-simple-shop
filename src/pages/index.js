2775909ec1bfedd510586dfede1144b77b00410f
maryamqasemi <maryamqasemi2022@gmail.com>
2023-03-13 11:19:45 +0330

    debug eslint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-export { default as Home } from "./Home/Home";
-export { default as About } from "./About/About";
-export { default as Contact } from "./Contact/Contact";
-export { default as Nopage } from "./Nopage/Nopage";
-export { default as Blog } from "./Blog/Blog";
-export { default as CartPage } from "./CartPage/CartPage";
-export { default as SingleProduct } from "./SingleProduct/SingleProduct";
+export { default as Home } from './Home/Home';
+export { default as About } from './About/About';
+export { default as Contact } from './Contact/Contact';
+export { default as Nopage } from './Nopage/Nopage';
+export { default as Blog } from './Blog/Blog';
+export { default as CartPage } from './CartPage/CartPage';
+export { default as SingleProduct } from './SingleProduct/SingleProduct';
